a1bb3420b5ab9787422047cf09060ed823e5f98d
Lyra-B <glykeriapeppa@gmail.com>
2015-02-27 17:25:41 +0000

    No access to pages unless logged in

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -3,22 +3,20 @@ require 'active_support'
 
 MyAmazingAddressBook::App.controllers :person do
   #Action Controller Overview '#8'
-  # before_action :require_login
-  # skip_before_action :require_login, only: [:login, :sign_up, :homepage]
-
-  # before :except => :homepage do
-  #   def require_login
-  #     # binding.pry
-  #     unless session[:logged_in] || request.path.include?('/login') || request.path.include?('/sign_up')
-  #       flash[:notice] = "You must be logged in to access this section"
-  #       redirect '/login' # halts request cycle
-  #     end
-  #   end
-  #   require_login
-  # end
+  before :except => :homepage do
+    def require_login
+      unless session[:logged_in] || request.path.include?('/login') || request.path.include?('/sign_up')
+        # binding.pry
+        flash[:notice] = "You must be logged in to access this section"
+        redirect '/person'
+      end
+    end
+    require_login
+  end
 
 
   get :homepage, :map => '' do
+    flash[:notice]
     render :'people/homepage'
   end
 
@@ -42,9 +40,7 @@ MyAmazingAddressBook::App.controllers :person do
   end
 
   post :login do
-    #binding.pry
     @user = User.find_by_username(params[:user][:username])
-    #binding.pry
     if @user && params[:user][:password] == @user.password
       session[:logged_in] = true
       flash[:notice] = "You are successfully logged in"
@@ -69,7 +65,7 @@ MyAmazingAddressBook::App.controllers :person do
 
   get :all do
   	@people = Person.all
-  	render :'people/all' #'/people/all'
+  	render :'people/all'
   end
 
   get :new do
@@ -79,17 +75,10 @@ MyAmazingAddressBook::App.controllers :person do
 
   post :add_fields, :map => '/person/add_fields' do
     @person = Person.new(params[:person])
-    #binding.pry
     render :'people/type'
-    # if @person.type == 'Trainee'
-    #   render :'people/trainee_form'
-    # elsif @person.type == 'Instructor'
-    #   render :'people/instructor_form'
-    # end
   end
 
 	post :create, :map => '' do
-    #binding.pry
 		@person = Person.new(params[:trainee] || params[:instructor])
     @person.save
 	  redirect "/person/#{@person.id}"
@@ -102,7 +91,6 @@ MyAmazingAddressBook::App.controllers :person do
 
   put :update, :map => 'person/:id' do
     @person = Person.find(params[:id])
-    #binding.pry
     @person.update(params[:trainee] || params[:instructor])
     redirect "/person/#{@person.id}"
   end
@@ -113,11 +101,7 @@ MyAmazingAddressBook::App.controllers :person do
   end
 
   get :lastname, :map => 'person/lastname/:letter' do
-    #binding.pry
     @people = Person.where("last_name LIKE '#{params[:letter]}%'")
-    # @people = Person.all.select do |person|
-    #   person.last_name.start_with?(params[:letter])
-    # end
     if @people.blank?
       flash[:notice] = "No matches!"
       redirect 'person/index'
@@ -126,7 +110,6 @@ MyAmazingAddressBook::App.controllers :person do
   end
 
   delete :destroy, :map => 'person/:id' do
-    #binding.pry
     @person = Person.find(params[:id])
     @person.destroy
     redirect 'person/index'
